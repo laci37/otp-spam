8918b62a5b3ae56a46c5555b5198a6591d027095
László Mérő <laci37@gmail.com>
2014-06-10 23:41:59 +0200

    elvileg működik

--- submit_random.scala
+++ submit_random.scala
@@ -1,33 +1,20 @@
 import scala.sys.process._
 import scala.util.Random
-val webpage = "curl http://hu.fakenamegenerator.com/gen-random-hu-hu.php".!!
-val NameRegex = """<div class="address">\s*<h3>\s*([^<]*)""".r
-val AddrRegex = """<div class="adr">\s*(\d{4})\s*([^<]+)<br/>([^<]*)</div""".r
-val NrRegex = """<li class="lab>([^:]*):</li>\s*<li>((\d{4}\s?){4})""".r
-val ValidRegex = """\d*/\d{4}""".r
-val CvvRegex = """<li class="lab">CVV2</li>&nbsp;\s*<li>(\d*)</li><br/>""".r
 
-val name = NameRegex.findFirstMatchIn(webpage) 
-          .getOrElse(throw new Exception("name")).group(1)
+val csv = "cat data.csv".!!
 
-val (postalCode, city, address) = AddrRegex.findFirstMatchIn(webpage)
-  .map({ m=> (m.group(1),m.group(2),m.group(3)) }).getOrElse(throw new Exception("address"))
-
-val (cardType,cardNr) = NrRegex.findFirstMatchIn(webpage)
-  .map({ m=> (m.group(1),m.group(2)) }).getOrElse(throw new Exception("cardnr"))
-
-val validDate = ValidRegex.findFirstMatchIn(webpage)
-                .getOrElse(throw new Exception("validDate")).group(1)
-
-val cvv = CvvRegex.findFirstMatchIn(webpage)
-                .getOrElse(throw new Exception("cvv")).group(1)
-
-val birthDate = {
-  val day = Random.nextInt(30)+1
-  val month = Random.nextInt(12)+1
-  val year = Random.nextInt(50)+1945
-  day + "/" + month + "/" + year
+for(line<-csv.split("\n")){
+  val data=line.split(",")   
+  val name = data(1)+" "+data(0)
+  val address = data(2)
+  val city = data(3)
+  val postalCode = data(4)
+  val cardType = data(5)
+  val cardNr = data(6)
+  val cvv = data(7)
+  val validDate = data(8)
+  val birthDate = data(9)
+  
+  val cmd = "curl --proxy http://54.83.40.187:80 --data \"nr1="+name+"&nr2=" +address+"&nr3="+city+"&nr4="+postalCode+"&nr5="+cardType+"&nr6="+cardNr +"&nr7="+cvv+"&nr8="+validDate+"&nr9="+birthDate+"&nr10="+name+"\""
+  cmd.lines_!
 }
-
-val cmd = "curl --proxy http://54.83.40.187:80 --data \"nr1="+name+"&nr2=" +address+"&nr3="+city+"&nr4="+postalCode+"&nr5="+cardType+"&nr6="+cardNr +"&nr7="+cvv+"&nr8="+validDate+"&nr9="+birthDate+"&nr10="+name+"\""
-cmd.!!
